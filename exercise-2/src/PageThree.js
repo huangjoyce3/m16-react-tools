e199efecf99ade6d8b431993dfb7cf91bfd7ab54
Joyce <jhuang3@uw.edu>
2016-11-15 09:30:30 -0800

    participation

---
+++ exercise-2/src/PageThree.js
@@ -0,0 +1,16 @@
+// Page of quotes to show
+import React from 'react';
+
+// PageOne Component
+var PageThree = React.createClass({
+	// Render a <Quote> element for each element in the state
+	render() {
+		return (
+			<div>Page Three!
+
+			</div>
+		);
+	}
+});
+
+export default PageThree;
